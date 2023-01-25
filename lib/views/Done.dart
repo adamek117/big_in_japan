48c906a5947085f30f8df780f4982a1a7833d29d
weronikachr <weronika.chruszcz.2000@gmail.com>
2023-01-25 23:05:34 +0100

    delete task from Done list on check

--- lib/views/Done.dart
+++ lib/views/Done.dart
@@ -2,6 +2,7 @@ import 'package:big_in_japan/models/done_tile.dart';
 import 'package:flutter/material.dart';
 import '../models/boards.dart';
 import 'package:big_in_japan/models/users.dart';
+import 'package:http/http.dart' as http;
 
 class Done extends StatefulWidget {
   final User user;
@@ -16,6 +17,8 @@ class Done extends StatefulWidget {
 class _DoneState extends State<Done> {
   final _controller = TextEditingController();
   List doneList = [];
+  String? boardId;
+  String? columnId;
 
   @override
   void initState() {
@@ -23,12 +26,14 @@ class _DoneState extends State<Done> {
     setState(() {
       if (widget.doneList.isNotEmpty) {
         Boards board = widget.doneList[0];
+        boardId = board.id;
         List? columns = board.columns
             ?.where((column) => column.name == 'done')
             .toList()
             .cast<dynamic>();
 
         if (columns is List && columns.isNotEmpty) {
+          columnId = columns[0].id;
           doneList = List.from(columns[0].tasks);
         }
       }
@@ -37,7 +42,11 @@ class _DoneState extends State<Done> {
 
   void checkBoxListChanged(bool? value, int index) {
     setState(() {
-      doneList[index][1] = !doneList[index][1];
+      final response = http.delete(
+        Uri.parse(
+            "http://localhost:3000/boards/${boardId}/columns/${columnId}/tasks/${doneList[index].id}"),
+        headers: {'x-user-id': widget.user.id},
+      );
     });
   }
 
@@ -55,7 +64,7 @@ class _DoneState extends State<Done> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text("Now"),
+        title: const Text("Done"),
         elevation: 0,
         backgroundColor: Colors.blueGrey,
       ),
